e8740077b6ff9440d6caf1f99bf8d9184c9f36e2
aKaquilala <aKaquilala@mcm.edu.ph>
2025-12-12 22:29:51 +0800

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -10,23 +10,3 @@ function countCharacters() {
     let newStr2 = str2.length;
     document.getElementById("countTxtResult").innerHTML = newStr2;
 }
-let txtInput = document.querySelector('#txtInput');
-let btnCheck = document.querySelector('#btnCheck');
-let txtOutput = document.querySelector('#txtOutput');
-
-// event
-btnCheck.addEventListener('click', function() {
-    // logic
-    let str = txtInput.value;
-    
-    // Use includes("@") method to check if the email contains the required character
-    if (str.includes("@")) {
-        txtOutput.innerText = "Valid";
-        txtOutput.className = "valid";
-        console.log('Email is valid:', str);
-    } else {
-        txtOutput.innerText = "Invalid";
-        txtOutput.className = "invalid";
-        console.log('Email is invalid:', str);
-    }
-});
