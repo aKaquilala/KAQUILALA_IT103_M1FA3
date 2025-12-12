ef83c6ce178b3c54637bfae42dfd29fdea92c060
aKaquilala <aKaquilala@mcm.edu.ph>
2025-12-12 22:20:45 +0800

    Create script.js

---
+++ asset/script.js
@@ -0,0 +1,12 @@
+function replaceSpace() {
+    let str1 = document.getElementById("replaceTxtInput").value;
+    let newStr1 = str1.replaceAll(' ', '');
+    document.getElementById("replaceTxtResult").innerHTML = newStr1;
+}
+
+function countCharacters() {
+    let str2 = document.getElementById("countTxtInput").value;
+    str2 = str2.trim();
+    let newStr2 = str2.length;
+    document.getElementById("countTxtResult").innerHTML = newStr2;
+}
